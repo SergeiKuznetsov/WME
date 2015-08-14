e488a5e511cf7059d3d4c1a8f595e14bcead54ad
Sergey Kuznetsov <votkinsk@gmail.com>
2015-08-14 16:21:56 +0400

    Update WME_Corrector.user.js

--- WME_Corrector.user.js
+++ WME_Corrector.user.js
@@ -99,7 +99,31 @@ function WME_Corrector_init() {
         var publicDictionnaryURL = '' ;
         waitForCountryTop();
   };
+	if ('undefined' == typeof __RTLM_PAGE_SCOPE_RUN__) {
+    (function page_scope_runner() {
+        // If we're _not_ already running in the page, grab the full source
+        // of this script.
+        var my_src = "(" + page_scope_runner.caller.toString() + ")();";
 
+        // Create a script node holding this script, plus a marker that lets us
+        // know we are running in the page scope (not the Greasemonkey sandbox).
+        // Note that we are intentionally *not* scope-wrapping here.
+        var script = document.createElement('script');
+        script.setAttribute("type", "text/javascript");
+        script.textContent = "var __RTLM_PAGE_SCOPE_RUN__ = true;\n" + my_src;
+
+        // Insert the script node into the page, so it will run, and immediately
+        // remove it to clean up.  Use setTimeout to force execution "outside" of
+        // the user script scope completely.
+        setTimeout(function() {
+            document.body.appendChild(script);
+            document.body.removeChild(script);
+        }, 0);
+    })();
+
+    // Stop running, because we know Greasemonkey actually runs us in
+    // an anonymous wrapper.
+    return;
 } // WME_Corrector_init
 
 // другие функции
