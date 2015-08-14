1bca2408d3b0ba6f4a1500c60660d8aaf7f1bde5
Sergey Kuznetsov <sergey@izhevsk.pro>
2015-08-14 17:38:01 +0400

    add }

--- WME_Corrector.user.js
+++ WME_Corrector.user.js
@@ -21,25 +21,19 @@ var WME_CRT_version = "0.1";
 function WME_Corrector_bootstrap() {
 	console_log("Wait...");
 	var bGreasemonkeyServiceDefined = false;
-	try
-	{
-		if ("object" === typeof Components.interfaces.gmIGreasemonkeyService)
-		{
+	try {
+		if ("object" === typeof Components.interfaces.gmIGreasemonkeyService) {
 			bGreasemonkeyServiceDefined = true;
 		}
-	}
-	catch (err)
-	{
+	} catch (err) {
 		// Ждем
 	}
-	if ( "undefined" === typeof unsafeWindow  ||  ! bGreasemonkeyServiceDefined)
-	{
-		unsafeWindow    = ( function ()
-		{
-			var dummyElem   = document.createElement('p');
-			dummyElem.setAttribute ('onclick', 'return window;');
-			return dummyElem.onclick ();
-		} ) ();
+	if ("undefined" === typeof unsafeWindow || !bGreasemonkeyServiceDefined) {
+		unsafeWindow = (function() {
+			var dummyElem = document.createElement('p');
+			dummyElem.setAttribute('onclick', 'return window;');
+			return dummyElem.onclick();
+		})();
 	}
 	/* можно начать выполнение кода! */
 	WME_Corrector_init();
@@ -47,95 +41,106 @@ function WME_Corrector_bootstrap() {
 
 function WME_Corrector_init() {
 	console_log("Init...");
+
 	function loadingRules() {
 		console_log("Loading rules...");
 		var myWaze = unsafeWindow.Waze;
 		if (myWaze && myWaze.model && myWaze.model.countries && myWaze.model.countries.top && myWaze.model.countries.top.id) {
-			var myCountryName = myWaze.model.countries.objects[myWaze.model.countries.top.id].name;  
-			switch(myWaze.model.countries.top.id) {
-				case 186:	// Russia
-                			unsafeWindow.mainDictionnaryKey = '18qup6nGuy6f0n0Jw-nCHcrJmH70aPJzYtkyfaWzZaPE';
-                			unsafeWindow.publicDictionnarykey = '1CLMGOuANq-1-XI4TYMoQ-Aqx5rlDXFPkZdryZt8IhT4';
-                			break;
-        			default:
-                			alert ("WME Corrector\n\nНе найдены правила для страны " + myCountryName + " " + myWaze.model.countries.top.id);
-                			unsafeWindow.WME_CRT_onload = "Error";
-                			delete WME_CRT_1_mainDictionaryTxt;
-                			delete WME_CRT_1_publicDictionaryTxt;
-                			return;
+			var myCountryName = myWaze.model.countries.objects[myWaze.model.countries.top.id].name;
+			switch (myWaze.model.countries.top.id) {
+				case 186: // Russia
+					unsafeWindow.mainDictionnaryKey = '18qup6nGuy6f0n0Jw-nCHcrJmH70aPJzYtkyfaWzZaPE';
+					unsafeWindow.publicDictionnarykey = '1CLMGOuANq-1-XI4TYMoQ-Aqx5rlDXFPkZdryZt8IhT4';
+					break;
+				default:
+					alert("WME Corrector\n\nНе найдены правила для страны " + myCountryName + " " + myWaze.model.countries.top.id);
+					unsafeWindow.WME_CRT_onload = "Error";
+					delete WME_CRT_1_mainDictionaryTxt;
+					delete WME_CRT_1_publicDictionaryTxt;
+					return;
 			}
-        GM_xmlhttpRequest({            
-            method:  'GET',
-            url: 	'https://docs.google.com/spreadsheets/d/' + unsafeWindow.mainDictionnaryKey +'/export?format=csv' ,      // безусловные правила
-            headers: {"User-Agent": "Mozilla/5.0", 
-                      "Accept": "text/plain"  },     
-            synchronous: false,
-            onload: function (mainDictionary) {
-                unsafeWindow.WME_CRN_1_mainDictionaryTxt = mainDictionary.responseText;
-            }
-        });
-        GM_xmlhttpRequest({
-            method:  'GET',
-            url: 'https://docs.google.com/spreadsheets/d/' + unsafeWindow.publicDictionnarykey +'/export?format=csv' ,      // опциональные правила
-            headers: {"User-Agent": "Mozilla/5.0",
-                      "Accept": "text/plain"  },
-            synchronous: false,
-            onload: function (publicDictionary) {
-                unsafeWindow.WME_CRT_1_publicDictionaryTxt = publicDictionary.responseText;
-            }
-        });
-        return myWaze.model.countries.top.id ;
-    }
-    else {
-        setTimeout (function() {loadingRules();}, 1000);
-    }
+			GM_xmlhttpRequest({
+				method: 'GET',
+				url: 'https://docs.google.com/spreadsheets/d/' + unsafeWindow.mainDictionnaryKey + '/export?format=csv', // безусловные правила
+				headers: {
+					"User-Agent": "Mozilla/5.0",
+					"Accept": "text/plain"
+				},
+				synchronous: false,
+				onload: function(mainDictionary) {
+					unsafeWindow.WME_CRN_1_mainDictionaryTxt = mainDictionary.responseText;
+				}
+			});
+			GM_xmlhttpRequest({
+				method: 'GET',
+				url: 'https://docs.google.com/spreadsheets/d/' + unsafeWindow.publicDictionnarykey + '/export?format=csv', // опциональные правила
+				headers: {
+					"User-Agent": "Mozilla/5.0",
+					"Accept": "text/plain"
+				},
+				synchronous: false,
+				onload: function(publicDictionary) {
+					unsafeWindow.WME_CRT_1_publicDictionaryTxt = publicDictionary.responseText;
+				}
+			});
+			return myWaze.model.countries.top.id;
+		} else {
+			setTimeout(function() {
+				loadingRules();
+			}, 1000);
+		}
 	} // loadingRules
 
-  if ('undefined' == typeof WME_CRT_onload) {       
-        unsafeWindow.WME_CRT_onload = "In Progress";
-        unsafeWindow.WME_CRT_1_mainDictionaryTxt = "In Progress";
-        unsafeWindow.WME_CRT_1_publicDictionaryTxt  = "In Progress";
-        var mainDictionnaryURL = '' ;
-        var publicDictionnaryURL = '' ;
-        waitForCountryTop();
-  };
+	if ('undefined' == typeof WME_CRT_onload) {
+		unsafeWindow.WME_CRT_onload = "In Progress";
+		unsafeWindow.WME_CRT_1_mainDictionaryTxt = "In Progress";
+		unsafeWindow.WME_CRT_1_publicDictionaryTxt = "In Progress";
+		var mainDictionnaryURL = '';
+		var publicDictionnaryURL = '';
+		waitForCountryTop();
+	};
 	if ('undefined' == typeof __RTLM_PAGE_SCOPE_RUN__) {
-    (function page_scope_runner() {
-        // If we're _not_ already running in the page, grab the full source
-        // of this script.
-        var my_src = "(" + page_scope_runner.caller.toString() + ")();";
+		(function page_scope_runner() {
+			// If we're _not_ already running in the page, grab the full source
+			// of this script.
+			var my_src = "(" + page_scope_runner.caller.toString() + ")();";
 
-        // Create a script node holding this script, plus a marker that lets us
-        // know we are running in the page scope (not the Greasemonkey sandbox).
-        // Note that we are intentionally *not* scope-wrapping here.
-        var script = document.createElement('script');
-        script.setAttribute("type", "text/javascript");
-        script.textContent = "var __RTLM_PAGE_SCOPE_RUN__ = true;\n" + my_src;
+			// Create a script node holding this script, plus a marker that lets us
+			// know we are running in the page scope (not the Greasemonkey sandbox).
+			// Note that we are intentionally *not* scope-wrapping here.
+			var script = document.createElement('script');
+			script.setAttribute("type", "text/javascript");
+			script.textContent = "var __RTLM_PAGE_SCOPE_RUN__ = true;\n" + my_src;
 
-        // Insert the script node into the page, so it will run, and immediately
-        // remove it to clean up.  Use setTimeout to force execution "outside" of
-        // the user script scope completely.
-        setTimeout(function() {
-            document.body.appendChild(script);
-            document.body.removeChild(script);
-        }, 0);
-    })();
+			// Insert the script node into the page, so it will run, and immediately
+			// remove it to clean up.  Use setTimeout to force execution "outside" of
+			// the user script scope completely.
+			setTimeout(function() {
+				document.body.appendChild(script);
+				document.body.removeChild(script);
+			}, 0);
+		})()
+	};
 
-    // Stop running, because we know Greasemonkey actually runs us in
-    // an anonymous wrapper.
-    return;
+	// Stop running, because we know Greasemonkey actually runs us in
+	// an anonymous wrapper.
+	return;
 } // WME_Corrector_init
 
 // другие функции
 
 // Лимит несохраненных правок еще не достигнут
-function limitForSaveNotReached() { 
+
+function limitForSaveNotReached() {
 	return (Waze.model.actionManager.index < 99);
 }
 
 // Отладочные сообщения
+
 function console_log(msg) {
-	if (console) { console.log("WME Corrector: " + msg);}
+	if (console) {
+		console.log("WME Corrector: " + msg);
+	}
 }
 
 // вызываем загрузчик в конце скрипта test
